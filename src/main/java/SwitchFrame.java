16c7170d912e8e9a6779717d5d2f242c93801eb1
Daniel <daniel.gru17@gmail.com>
2020-04-05 11:56:57 +0300

    Commit java class2

--- src/main/java/SwitchFrame.java
+++ src/main/java/SwitchFrame.java
@@ -1,6 +1,4 @@
-package switchto;
 
-import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
 import org.junit.Before;
@@ -9,6 +7,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class SwitchFrame {
     private WebDriver driver;
